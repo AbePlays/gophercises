cb0db1140e172df4cd598af4e16c9787e3fd069c
Abhishek Rawat <abhi.rawat456@gmail.com>
2026-09-13 12:10:46 +0530

    chore: add solution

--- 15-recover-chroma/go.mod
+++ 15-recover-chroma/go.mod
@@ -1,3 +1,8 @@
 module github.com/AbePlays/gophercises/15-recover-chroma
 
 go 1.27.1
+
+require (
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/dlclark/regexp2 v1.12.0 // indirect
+)
--- 15-recover-chroma/main.go
+++ 15-recover-chroma/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"runtime/debug"
+	"strconv"
+	"strings"
+
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 )
 
 func main() {
@@ -12,7 +22,43 @@ func main() {
 	mux.HandleFunc("/panic/", panicDemo)
 	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/debug/", sourceCodeHandler)
 	log.Fatal(http.ListenAndServe(":3000", devMw(mux)))
+}
+
+func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.FormValue("path")
+	lineStr := r.FormValue("line")
+
+	line, err := strconv.Atoi(lineStr)
+	if err != nil {
+		line = -1
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	b := bytes.NewBuffer(nil)
+	_, err = io.Copy(b, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var lines [][2]int
+	if line > 0 {
+		lines = append(lines, [2]int{line, line})
+	}
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, b.String())
+	style := styles.Get("github")
+
+	w.Header().Set("Content-Type", "text/html")
+	formatter := html.New(html.TabWidth(2), html.WithLineNumbers(true), html.HighlightLines(lines))
+	formatter.Format(w, style, iterator)
 }
 
 func devMw(app http.Handler) http.HandlerFunc {
@@ -23,7 +69,7 @@ func devMw(app http.Handler) http.HandlerFunc {
 				stack := debug.Stack()
 				log.Println(string(stack))
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, string(stack))
+				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, makeLinks(string(stack)))
 			}
 		}()
 		app.ServeHTTP(w, r)
@@ -45,4 +91,39 @@ func funcThatPanics() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
+
+func makeLinks(stack string) string {
+	lines := strings.Split(stack, "\n")
+
+	for i, line := range lines {
+		if len(line) == 0 || line[0] != '\t' {
+			continue
+		}
+
+		file := ""
+		for i, ch := range line {
+			if ch == ':' {
+				file = line[1:i]
+				break
+			}
+		}
+
+		var lineStr strings.Builder
+		for idx := len(file) + 2; idx < len(line); idx++ {
+			if line[idx] < '0' || line[idx] > '9' {
+				break
+			}
+
+			lineStr.WriteByte(line[idx])
+		}
+
+		v := url.Values{}
+		v.Set("path", file)
+		v.Set("line", lineStr.String())
+
+		lines[i] = "\t<a href=\"/debug?" + v.Encode() + "\">" + file + ":" + lineStr.String() + "</a>" + line[len(file)+2+len(lineStr.String()):]
+	}
+
+	return strings.Join(lines, "\n")
 }
