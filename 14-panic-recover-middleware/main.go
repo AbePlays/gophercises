6e5168f894c56abc04a5fedbd622de1225633874
Abhishek Rawat <abhi.rawat456@gmail.com>
2026-09-13 11:02:25 +0530

    chore: add solution

--- 14-panic-recover-middleware/main.go
+++ 14-panic-recover-middleware/main.go
@@ -1,17 +1,96 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"runtime/debug"
 )
+
+// type ResonseWriter interface {
+// 	Header() http.Header
+// 	Write([]byte) (int, error)
+// 	WriteHeader(statusCode int)
+// }
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hijacker, ok := rw.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, fmt.Errorf("hijacking not supported")
+	}
+
+	return hijacker.Hijack()
+}
+
+func (rw *responseWriter) Flush() {
+	flusher, ok := rw.ResponseWriter.(http.Flusher)
+	if ok {
+		flusher.Flush()
+	}
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/panic/", panicDemo)
 	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux, true)))
+}
+
+func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				stack := debug.Stack()
+				log.Println(string(stack))
+
+				if !dev {
+					http.Error(w, "Something went wrong :(", http.StatusInternalServerError)
+					return
+				}
+
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintf(w, "<h1>panic: %s</h1><pre>%s<pre>", err, string(stack))
+			}
+		}()
+
+		newW := &responseWriter{ResponseWriter: w}
+		app.ServeHTTP(newW, r)
+		newW.flush()
+	}
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
